60ea60d1d45b698b8cec2f77b8e2505e1328025a
Yuuta Watanabe <yuutakkuma.74@gmail.com>
2024-02-02 21:49:44 +0900

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+const root = document.getElementById('root')
+const text = document.createElement('h2')
+text.textContent = 'Hello world'
+root?.appendChild(text)
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devServer: {
+    static: './public'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
+  }
+}
