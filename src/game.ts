3eca0ef83b91e09b5e81e0751c0c12e4cae6e9e3
Yuuta Watanabe <yuutakkuma.74@gmail.com>
2024-02-04 00:28:18 +0900

    スペア判定を追加

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,11 @@
 import { Storage } from './storage'
 
-type UpdateScore = {
+type Score = {
   score: string | number
 }
 
-type LastRound = {
-  score: string | number
+type UpdateScore = Score & {
+  round: number
 }
 
 export class Game {
@@ -21,6 +21,12 @@ export class Game {
   private currentScoreText: HTMLHeadingElement
 
   private currentCountText: HTMLHeadingElement
+
+  /** 前回スペアだったかの判定 */
+  private isSpare: boolean = false
+
+  /** 前ラウンドのスコア */
+  private beforeRoundScore: number = 0
 
   constructor() {
     // スコアElement一覧を取得
@@ -44,11 +50,15 @@ export class Game {
   start() {
     // 投球ボタンへ反映
     const bolingBtn = document.querySelector('.bowling_btn')
-    bolingBtn?.addEventListener('click', this.bowling)
+    bolingBtn?.addEventListener('click', () => {
+      this.bowling()
+    })
 
     // リセットボタンへ反映
     const resetBtn = document.querySelector('.reset_btn')
-    resetBtn?.addEventListener('click', this.reset)
+    resetBtn?.addEventListener('click', () => {
+      this.reset()
+    })
   }
 
   /**
@@ -57,6 +67,8 @@ export class Game {
    */
   private getRandomNum(num: { max: number }) {
     const { max } = num
+    console.log({ max })
+
     return Math.floor(Math.random() * max + 1)
   }
 
@@ -65,9 +77,16 @@ export class Game {
    * スコアを更新する
    */
   private updateScore(item: UpdateScore) {
-    const { score } = item
-    Storage.save({ round: this.round, score })
-    this.scoresElement[this.round].textContent = score.toString()
+    const { round, score } = item
+    Storage.save({ round, score })
+    this.scoresElement[round].textContent = score.toString()
+
+    if (this.isSpare) {
+      const updatedBeforeRoundScore = this.beforeRoundScore + Number(score)
+      Storage.save({ round: round - 1, score: updatedBeforeRoundScore })
+      this.scoresElement[round - 1].textContent = updatedBeforeRoundScore.toString()
+      this.isSpare = false
+    }
   }
 
   /**
@@ -95,23 +114,44 @@ export class Game {
 
     // ストライク判定
     if (this.count === 1 && randomScore === 10) {
-      this.updateScore({ score: randomScore })
-      this.round += 1
+      this.updateScore({ round: this.round, score: randomScore })
+      this.updateRound({ score: randomScore })
       return
     }
 
     // 1球目且つ10未満
     if (this.count === 1 && randomScore < 10) {
-      this.updateScore({ score: randomScore })
+      this.updateScore({ round: this.round, score: randomScore })
       this.count = 2
       return
     }
 
-    // 二球目
+    /*
+     * 2球目且つスペア
+     * - スペアを取った場合、次の投球分を加算することができる
+     * @example
+     * ラウンド1 - 1 => 3
+     * ラウンド1 - 2 => 7 スペア！！
+     *
+     * ラウンド2 - 1 => 5 この時点でラウンド1の得点 = 15となる
+     *
+     */
     const firstScore = Storage.data[this.round]
+    if (this.count === 2 && typeof firstScore === 'number' && firstScore + randomScore === 10) {
+      const score = firstScore + randomScore
+      this.updateScore({ round: this.round, score })
+      this.updateRound({ score })
+      this.count = 1
+      this.isSpare = true
+      this.beforeRoundScore = score
+      return
+    }
+
+    // 2球目
     if (this.count === 2 && typeof firstScore === 'number') {
-      this.updateScore({ score: firstScore + randomScore })
-      this.round += 1
+      const score = firstScore + randomScore
+      this.updateScore({ round: this.round, score })
+      this.updateRound({ score })
       this.count = 1
       return
     }
@@ -121,11 +161,11 @@ export class Game {
    * @description
    * 最終ラウンドの処理
    */
-  private lastRound(item: LastRound) {
+  private lastRound(item: Score) {
     const { score } = item
     // 1球目
     if (this.count === 1) {
-      this.updateScore({ score })
+      this.updateScore({ round: this.round, score })
       this.count = 2
       return
     }
@@ -135,15 +175,15 @@ export class Game {
     const isNextCount = incremented >= 10
     // 2球目で且つ3球目がある場合
     if (this.count === 2 && isNextCount) {
-      this.updateScore({ score: incremented })
+      this.updateScore({ round: this.round, score: incremented })
       this.count = 3
       return
     }
 
     // 2球目で且つ3球目が無い 又は3球目
-    this.updateScore({ score: incremented })
+    this.updateScore({ round: this.round, score: incremented })
+    this.updateRound({ score })
     this.count = 1
-    this.round += 1
     return
   }
 
@@ -154,11 +194,12 @@ export class Game {
   private reset() {
     this.count = 1
     this.round = 0
-    Storage.reset()
+    this.beforeRoundScore = 0
     this.scoresElement.forEach(element => {
       element.textContent = ''
     })
     this.updateDescription(undefined)
+    Storage.reset()
   }
 
   /**
@@ -171,10 +212,18 @@ export class Game {
   }
 
   private updateDescription(randomScore: number | undefined) {
-    console.log(this.count)
-
     this.currentRoundText.textContent = `ラウンド ${this.round + 1}`
     this.currentCountText.textContent = `投球 ${this.count}回目`
     this.currentScoreText.textContent = randomScore ? `今の投球結果: ${randomScore}` : '投球ボタンを押してね！'
+  }
+
+  /**
+   * @description
+   * ラウンドを更新し、前ラウンドスコアを更新します。
+   */
+  private updateRound(item: Score) {
+    const { score } = item
+    this.round += 1
+    this.beforeRoundScore = Number(score)
   }
 }
